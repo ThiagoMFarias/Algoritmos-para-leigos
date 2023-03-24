9bb7e7def5ea681cb4429e0df317697d7fb15742
Thiago <FARIAS_CE@YAHOO.COM.BR>
2023-03-23 23:28:59 -0300

    Create Algo_leigos_Bubble_sort.py

---
+++ Package download - exercises/Algo_leigos_Bubble_sort.py
@@ -0,0 +1,22 @@
+vet = [21,9,1,45,11]
+
+for a in range(0, len(vet)):
+    print(vet[a])
+
+for i in range(0, len(vet)):
+    for x in range(0, 4)  :  
+        if vet[x] > vet[x+1]:
+            aux = vet[x]
+            vet[x] = vet[x+1]
+            vet[x+1] = aux
+            print(vet)
+
+print('Vetor Ordenado')
+for a in range(0, len(vet)):
+    print(vet[a])
+
+"""Vamos para as explicações:
+
+1- se eu quiser que o código não imprima os vetores antes de estarem ordenados, basta eu retirar a linha 12;
+2- na linha 7, se eu colocar range(0, len(vet)) ele irá dar erro em virtude de não existir o vetor 5, logo tive que restringir até o 4º vetor;
+3- aqui eu optei por selecionar o maior item e colocá-lo na fim da lista, porém tem a possibilidade de escolher o menor item e colocá-lo no início da lista, bastando trocar o sinal de > por < na linha 8;"""
